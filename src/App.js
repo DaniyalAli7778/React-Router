9de6472d8f572c2e36ab3166bbef1199fc796c97
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-15 15:33:06 +0500

    "add Tailwind css "

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
- 
+import './index.css'
+
 function App() {
   return (
- <><h1>
-  hellow World</h1></>
-     
+    <> 
+    <h1 className="text-yellow-500 text-3xl " >Hellow World</h1>
+    </>
   );
 }
 
