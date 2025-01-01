55927b0e9ea17165fc927facc12e61397c7f93ef
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2025-01-01 15:18:12 +0500

    "add GitHub page "

---
+++ src/Components/GitHub/GitHub.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+ export default function GitHub(){
+  return(
+    <>
+      <div  className='bg-gray-600 text-white text-3xl p-4'> Github Followers</div>
+
+    </>
+  )
+}
